af94ad9804270e138e2fda2273e51890eb7e49c1
alvintang0925 <alvintang0925@gmail.com>
2021-02-22 00:59:26 +0800

    Initial Commit

---
+++ Proofread_QTS_File/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Proofread_QTS_File
+//
+//  Created by 唐健恆 on 2021/2/22.
+//  Copyright © 2021 Alvin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
